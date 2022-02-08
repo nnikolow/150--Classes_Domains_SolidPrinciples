d2fa96893cfa4374daa5c6c88cf31d316087b962
Nikolay.Nikolov2 <nikolay.nikolov2@adastragrp.com>
2022-02-08 18:20:31 +0200

    fix

--- main.py
+++ main.py
@@ -11,31 +11,29 @@ import psycopg2
 DATA SOURCE : SIMULATION
 '''
 
-'''
-fake = Faker()
 
-# random key generator
-def rand_key():
-    num = str(random.randint(0, 10))
-    characters = ''.join(random.choice(string.ascii_uppercase) for i in range(3))
-    result = num + characters
-    return result
+def data_source_simulation():
+    fake = Faker()
 
+# random "key" generator
+    def rand_key():
+        num = str(random.randint(0, 10))
+        characters = ''.join(random.choice(string.ascii_uppercase) for i in range(3))
+        result = num + characters
+        return result
 
 # random json generator
-def input_data():
-    rand_data = {'key': rand_key(),
+    def input_data():
+        rand_data = {'key': rand_key(),
                     'value': round(random.uniform(0, 50), 1),
                     'ts': str(fake.date_time())}
-    print(rand_data)
 
-    with open('rand_data.json', 'w') as fp:
-        json.dump(rand_data, fp)
-    return rand_data
+        with open('rand_data.json', 'w') as fp:
+            json.dump(rand_data, fp)
+        return rand_data
 
-
-file = input_data()
-'''
+    file = input_data()
+    return file
 
 
 '''
@@ -43,37 +41,91 @@ DATA SOURCE: JSON FILE
 '''
 
 
-# def json_file():
-#     with open('json_template', encoding='utf-8', errors='ignore') as json_data:
-#         data = json.load(json_data)
-#         return data
-#
-#
-# # def print_to_stdout(*a):
-# #     print(*a, file=sys.stdout)
-#
-#
-# x = json_file()
-# print(x)
-# # print_to_stdout(x)
+def data_source_json_file():
+    def json_file():
+        with open('json_template', encoding='utf-8', errors='ignore') as json_data:
+            data = json.load(json_data)
+            return data
+
+    def print_to_stdout(*a):
+        print(*a, file=sys.stdout)
+
+    x = json_file()
+    return x
+
 
 '''
 SINK: POSTGRESQL
 '''
 
-connection = psycopg2.connect("dbname=db1 user=postgres password=postgres")
-cursor = connection.cursor()
-cursor.execute("set search_path to public")
 
-print("Opening file...\n")
-with open('json_template') as file:
-    data = file.read()
-print("File successfully loaded... \n")
-query_sql = """
-insert into table1 select * from
-json_populate_recordset(NULL::table1, %s);
-"""
+class DataSinkPostgres:
 
-cursor.execute(query_sql, (data,))
-connection.commit()
-print("File successfully loaded in database.")
+    def __init__(self):
+        self.connection = psycopg2.connect("dbname=db1 user=postgres password=postgres")
+        self.cursor = self.connection.cursor()
+        self.cursor.execute("set search_path to public")
+
+    def file(self):
+        print("Opening file...\n")
+        with open('json_template2') as file:
+            data = file.read()
+        print("File successfully loaded! \n")
+        query_sql = """
+        insert into table1 select * from
+        json_populate_recordset(NULL::table1, %s);
+        """
+
+        self.cursor.execute(query_sql, (data,))
+        self.connection.commit()
+
+    def simulation(self):
+        print("Opening file...\n")
+        with open('rand_data.json') as file:
+            data = file.read()
+        print("File successfully loaded! \n")
+        query_sql = """
+        insert into table1 select * from
+        json_populate_recordset(NULL::table1, %s);
+        """
+
+        self.cursor.execute(query_sql, (data,))
+        self.connection.commit()
+
+
+# p = DataSinkPostgres()
+# p.simulation()
+
+
+cycle = True
+data_source = input("Hello, which data source do you want to use? \n Simulation (s) or File (f)?\n")
+while cycle:
+    if data_source == "s":
+        data_sink = input("You've chosen Simulation! Where do you want to put the data? Console (c) or Postgres (p)?\n")
+        while cycle:
+            if data_sink == "c":
+                result = data_source_simulation()
+                print(result)
+                cycle = False
+            elif data_sink == "p":
+                p = DataSinkPostgres()
+                p.simulation()
+                cycle = False
+            else:
+                print("Please, choose an option between Console (c) or Postgres (p)")
+
+    elif data_source == "f":
+        data_sink = input("You've chosen File! Where do you want to put the data? Console (c) or Postgres (p)?\n")
+        while cycle:
+            if data_sink == "c":
+                result = data_source_json_file()
+                print(result)
+                cycle = False
+            elif data_sink == "p":
+                p = DataSinkPostgres()
+                p.file()
+                cycle = False
+            else:
+                print("Please, choose an option between Console (c) or Postgres (p)")
+
+
