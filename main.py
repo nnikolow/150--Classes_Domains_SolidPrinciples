9ccbb323b91d0504f19bade2db57e7661c2742d6
Nikolay.Nikolov2 <nikolay.nikolov2@adastragrp.com>
2022-02-09 15:43:38 +0200

    final

--- main.py
+++ main.py
@@ -1,57 +1,52 @@
 import json
-import string
 import sys
 from faker import Faker
 import random
-import datetime
 import string
 import psycopg2
 
 '''
-DATA SOURCE : SIMULATION
+DATA SOURCE : SIMULATION & FILE
 '''
 
 
-def data_source_simulation():
-    fake = Faker()
+class DataSource:
+    def data_source_simulation(self):
+        fake = Faker()
 
-# random "key" generator
-    def rand_key():
-        num = str(random.randint(0, 10))
-        characters = ''.join(random.choice(string.ascii_uppercase) for i in range(3))
-        result = num + characters
-        return result
+    # random json generator
+        def input_data(self):
+            def rand_key():
+                num = str(random.randint(0, 10))
+                characters = ''.join(random.choice(string.ascii_uppercase) for i in range(3))
+                result = num + characters
+                return result
+            rand_data = {'key': rand_key(),
+                        'value': round(random.uniform(0, 50), 1),
+                        'ts': str(fake.date_time())}
 
-# random json generator
-    def input_data():
-        rand_data = {'key': rand_key(),
-                    'value': round(random.uniform(0, 50), 1),
-                    'ts': str(fake.date_time())}
+            with open('rand_data.json', 'w') as fp:
+                json.dump(rand_data, fp)
+            return rand_data
 
-        with open('rand_data.json', 'w') as fp:
-            json.dump(rand_data, fp)
-        return rand_data
+        file = input_data("file")
+        return file
 
-    file = input_data()
-    return file
+    '''
+    DATA SOURCE: JSON FILE
+    '''
 
+    def data_source_json_file(self):
+        def json_file():
+            with open('json_template', encoding='utf-8', errors='ignore') as json_data:
+                data = json.load(json_data)
+                return data
 
-'''
-DATA SOURCE: JSON FILE
-'''
+        def print_to_stdout(*a):
+            print(*a, file=sys.stdout)
 
-
-def data_source_json_file():
-    def json_file():
-        with open('json_template', encoding='utf-8', errors='ignore') as json_data:
-            data = json.load(json_data)
-            return data
-
-    def print_to_stdout(*a):
-        print(*a, file=sys.stdout)
-
-    x = json_file()
-    return x
+        x = json_file()
+        return x
 
 
 '''
@@ -80,52 +75,56 @@ class DataSinkPostgres:
         self.connection.commit()
 
     def simulation(self):
-        print("Opening file...\n")
-        with open('rand_data.json') as file:
-            data = file.read()
-        print("File successfully loaded! \n")
-        query_sql = """
-        insert into table1 select * from
-        json_populate_recordset(NULL::table1, %s);
-        """
+        try:
+            print("Opening file...\n")
+            with open('rand_data.json') as file:
+                data = file.read()
 
-        self.cursor.execute(query_sql, (data,))
-        self.connection.commit()
+            query_sql = """
+            insert into table1 select * from
+            json_populate_recordset(NULL::table1, %s);
+            """
+
+            self.cursor.execute(query_sql, (data,))
+            self.connection.commit()
+            print("File successfully loaded! \n")
+        except psycopg2.errors.InvalidParameterValue:
+            print("Error in the JSON file -> reformat the file and try again.")
 
 
-# p = DataSinkPostgres()
-# p.simulation()
+while True:
+    data_source = input("Hello, which data source do you want to use? \n Simulation (s) or File (f)?\n")
 
-
-cycle = True
-data_source = input("Hello, which data source do you want to use? \n Simulation (s) or File (f)?\n")
-while cycle:
     if data_source == "s":
-        data_sink = input("You've chosen Simulation! Where do you want to put the data? Console (c) or Postgres (p)?\n")
-        while cycle:
+        while True:
+            data_sink = input(
+                "You've chosen Simulation! Where do you want to send the data? Console (c) or Postgres (p)?\n")
             if data_sink == "c":
-                result = data_source_simulation()
-                print(result)
-                cycle = False
+                r = DataSource()
+                print(r.data_source_simulation())
+                break
             elif data_sink == "p":
                 p = DataSinkPostgres()
                 p.simulation()
-                cycle = False
+                break
             else:
-                print("Please, choose an option between Console (c) or Postgres (p)")
+                print("Please, enter 'c' for Console or 'p' for Postgres.\n")
+        break
 
     elif data_source == "f":
-        data_sink = input("You've chosen File! Where do you want to put the data? Console (c) or Postgres (p)?\n")
-        while cycle:
+        while True:
+            data_sink = input("You've chosen File! Where do you want to send the data? Console (c) or Postgres (p)?\n")
             if data_sink == "c":
-                result = data_source_json_file()
-                print(result)
-                cycle = False
+                r = DataSource()
+                print(r.data_source_json_file())
+                break
             elif data_sink == "p":
                 p = DataSinkPostgres()
                 p.file()
-                cycle = False
+                break
             else:
-                print("Please, choose an option between Console (c) or Postgres (p)")
-
+                print("Choose an option for the sink between 'c' for Console and 'p' for Postgres")
+        break
+    else:
+        print("Please, enter 's' for Simulation of 'f' for File!\n")
 
